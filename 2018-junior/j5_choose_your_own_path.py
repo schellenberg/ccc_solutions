9051d089490335181237933577fa1f0c1a32d2a7
Dan Schellenberg <schellenberg@gmail.com>
2019-02-05 17:33:38 -0600

    Update j5_choose_your_own_path.py

--- 2018-junior/j5_choose_your_own_path.py
+++ 2018-junior/j5_choose_your_own_path.py
@@ -18,9 +18,44 @@ def are_all_pages_reachable(pages):
             if possible_page not in visited_pages and possible_page not in still_need_to_visit and possible_page != 0:
                 still_need_to_visit.append(possible_page)
     
-    return len(visited_pages) == pages[0]
+    if len(visited_pages) == pages[0]:
+        return "Y"
+    else:
+        return "N"
     
+
+def breadth_first_shortest_path(pages):
+    # populate with first level from page 1
+    levels = []
+    levels.append(extract_pages_to_include(pages[1]))
     
+    current_depth = 0
+    still_searching = True
+    while still_searching:
+        pages_to_visit = []
+        for page in levels[current_depth]:
+            linked_pages = extract_pages_to_include(pages[page])
+            for possible_page in linked_pages:
+                if possible_page not in pages_to_visit:
+                    pages_to_visit.append(possible_page)
+        levels.append(pages_to_visit)
+    
+        #check if we've found a path out!
+        if 0 in levels[-1]:
+            still_searching = False
+        else:
+            current_depth += 1
+    
+    return len(levels)
+            
+
+def extract_pages_to_include(page_data):
+    if page_data[0] == 0:
+        return [0]
+    else:
+        return page_data[1:]
+        
+
 def load_book():
     pages = []
 
@@ -41,3 +76,4 @@ def load_book():
 pages = load_book()
 #print(pages)
 print(are_all_pages_reachable(pages))
+print(breadth_first_shortest_path(pages))
